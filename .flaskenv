bf23fecd5f65cbf4c928b4aedbf489cbfdb28478
nicklong98 <nick.long@rtslabs.com>
2020-07-01 17:17:08 -0400

    can follow and unfollow users!

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,5 @@
 FLASK_APP=microblog.py
 FLASK_ENV=development
+FLASK_DEBUG=1
+MAIL_SERVER=localhost
+MAIL_PORT=8025
--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,6 @@
-from flask import flash
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
 from app.models import User
 
 
@@ -27,3 +26,22 @@ class RegistrationForm(FlaskForm):
     def validate_email(self, email):
         if User.query.filter_by(email=email.data).first() is not None:
             raise ValidationError('Please use a different email address.')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About Me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            if User.query.filter_by(username=self.username.data).first() is not None:
+                raise ValidationError('Please enter a different username')
+
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
